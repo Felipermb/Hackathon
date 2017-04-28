3dce2cb518e2746186db3645719f3a59f630616e
Kleyson Morais <kleysonb@hotmail.com>
2017-04-28 17:19:21 -0600

    Bugs merge

--- af2-lists2/src/pages/home/home.ts
+++ af2-lists2/src/pages/home/home.ts
@@ -1,16 +1,10 @@
 //importes de componente exerto
-<<<<<<< HEAD
-import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
-import { AngularFire } from 'angularfire2';
-import { Component } from '@angular/core';
-import firebase from 'firebase';
-=======
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import firebase from 'firebase';
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, AlertController, ActionSheetController, Platform } from 'ionic-angular';
 // import { Geolocation } from '@ionic-native/geolocation';
 // import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
->>>>>>> refs/remotes/origin/master
 
 import { AuthService } from '../../providers/auth-service';
 import { LoginPage } from './../login/login';
@@ -23,42 +17,36 @@ declare var google;
 })
 export class HomePage {
 
-<<<<<<< HEAD
   //musicas: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public authService: AuthService, public alertCtrl: AlertController, af: AngularFire, public actionSheetCtrl: ActionSheetController) {
-    //Acessando Banco de Dados
-    //console.log(af.database.list('/musicas'));
-    //this.musicas = af.database.list('/musicas');
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (!user) {
-        navCtrl.setRoot(LoginPage);
-      }
-    });
-=======
+
   @ViewChild('map', { read: ElementRef }) mapElement: ElementRef;
   map: any;
   musicas: FirebaseListObservable<any>;
 
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, platform: Platform,
-    af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+    af: AngularFire, public actionSheetCtrl: ActionSheetController, public authService:  AuthService  ) {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (!user) {
+        navCtrl.setRoot(LoginPage);
+      }
+    });
+
     platform.ready().then(() => {
       this.loadMap();
     });
+
+
     //Acessando Banco de Dados
     console.log(af.database.list('/musicas'));
     this.musicas = af.database.list('/musicas');
->>>>>>> refs/remotes/origin/master
   }
 
   logout() {
     this.authService.doLogout();
   }
 
-<<<<<<< HEAD
-  /*mostrarOpcoes(musicaId, musicaTitulo){
-=======
   loadMap(){
 
     let latLng = new google.maps.LatLng(-10.283783, -48.291471);
@@ -81,56 +69,59 @@ export class HomePage {
       position: latLng
     });
 
-    // let content = "<h4>Information!</h4>";          
+    // let content = "<h4>Information!</h4>";
 
     // this.addInfoWindow(marker, content);
 
   }
 
+  /*mostrarOpcoes(musicaId, musicaTitulo){
+
+
+
   mostrarOpcoes(musicaId, musicaTitulo){
->>>>>>> refs/remotes/origin/master
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'O que você quer fazer?',
-      buttons: [
-      {
-        text: 'Deletar Música',
-        role: 'destructive',
-        handler: () => {
-          this.removerMusica(musicaId);
-        }
-      },{
-        text: 'Atualizar Música',
-        handler: () => {
-          this.atualizarMusica(musicaId, musicaTitulo);
-        }
-      },{
-        text: 'Cancelar',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancelar clicado');
-        }
+let actionSheet = this.actionSheetCtrl.create({
+  title: 'O que você quer fazer?',
+  buttons: [
+    {
+      text: 'Deletar Música',
+      role: 'destructive',
+      handler: () => {
+        this.removerMusica(musicaId);
       }
-      ]
-    });
-    actionSheet.present();
+    }, {
+      text: 'Atualizar Música',
+      handler: () => {
+        this.atualizarMusica(musicaId, musicaTitulo);
+      }
+    }, {
+      text: 'Cancelar',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancelar clicado');
+      }
+    }
+  ]
+});
+actionSheet.present();
   }
 
-  removerMusica(musicaId: string){
-    this.musicas.remove(musicaId);
-  }
+removerMusica(musicaId: string){
+  this.musicas.remove(musicaId);
+}
 
-  atualizarMusica(musicaId: string, musicaTitulo: string){
-    let prompt = this.alertCtrl.create({
-      title: 'Noma da Música',
-      message: "Atualizar Música",
-      inputs: [
+atualizarMusica(musicaId: string, musicaTitulo: string){
+  let prompt = this.alertCtrl.create({
+    title: 'Noma da Música',
+    message: "Atualizar Música",
+    inputs: [
       {
         name: 'tituloVar',
         placeholder: 'Título',
         value: musicaTitulo
       },
-      ],
-      buttons: [
+    ],
+    buttons: [
       {
         text: 'Cancelar',
         handler: data => {
@@ -145,52 +136,22 @@ export class HomePage {
           });
         }
       }
-      ]
-    });
-    prompt.present();
-  }
+    ]
+  });
+  prompt.present();
+}
 
-  adicionarMusicas(){
-<<<<<<< HEAD
-        let prompt = this.alertCtrl.create({
-        title: 'Nome da Música',
-        message: "Entre com um nome para esta nova música ser adicionada",
-        inputs: [
-          {
-            name: 'titulo',
-            placeholder: 'Título'
-          },
-        ],
-        buttons: [
-          {
-            text: 'Cancelar',
-            handler: data => {
-              console.log('Cancelar Clicado');
-            }
-          },
-          {
-            text: 'Salvar',
-            handler: data => {
-              this.musicas.push({
-                titulo: data.titulo
-              });
-            }
-          }
-        ]
-      });
-      prompt.present();
-  }*/
-=======
-    let prompt = this.alertCtrl.create({
-      title: 'Nome da Música',
-      message: "Entre com um nome para esta nova música ser adicionada",
-      inputs: [
+adicionarMusicas(){
+  let prompt = this.alertCtrl.create({
+    title: 'Nome da Música',
+    message: "Entre com um nome para esta nova música ser adicionada",
+    inputs: [
       {
         name: 'titulo',
         placeholder: 'Título'
       },
-      ],
-      buttons: [
+    ],
+    buttons: [
       {
         text: 'Cancelar',
         handler: data => {
@@ -205,9 +166,9 @@ export class HomePage {
           });
         }
       }
-      ]
-    });
-    prompt.present();
-  }
->>>>>>> refs/remotes/origin/master
+    ]
+  });
+  prompt.present();
+}*/
+
 }
