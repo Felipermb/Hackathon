e26d4025a8e58ed2a7b289b97bee05361a858d97
felipermb <felipe_rdmd@hotmail.com>
2017-04-28 16:59:16 -0300

    Mapa

--- af2-lists2/src/pages/home/home.ts
+++ af2-lists2/src/pages/home/home.ts
@@ -1,9 +1,13 @@
 //importes de componente exerto
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import { Component } from '@angular/core';
-import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, AlertController, ActionSheetController, Platform } from 'ionic-angular';
+// import { Geolocation } from '@ionic-native/geolocation';
+// import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
 
 import { LoginPage } from './../login/login';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -11,40 +15,75 @@ import { LoginPage } from './../login/login';
 })
 export class HomePage {
 
+  @ViewChild('map', { read: ElementRef }) mapElement: ElementRef;
+  map: any;
   musicas: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire, public actionSheetCtrl: ActionSheetController) {
-      //Acessando Banco de Dados
-      console.log(af.database.list('/musicas'));
-      this.musicas = af.database.list('/musicas');
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, platform: Platform,
+    af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+    platform.ready().then(() => {
+      this.loadMap();
+    });
+    //Acessando Banco de Dados
+    console.log(af.database.list('/musicas'));
+    this.musicas = af.database.list('/musicas');
   }
 
   login(){
     this.navCtrl.push(LoginPage);
   }
 
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-10.283783, -48.291471);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+  }
+
+  addMarker(){
+
+    let latLng = new google.maps.LatLng(-10.283783, -48.291471);
+    let marker = new google.maps.Marker({
+      map: this.map,
+      position: latLng
+    });
+
+    // let content = "<h4>Information!</h4>";          
+
+    // this.addInfoWindow(marker, content);
+
+  }
+
   mostrarOpcoes(musicaId, musicaTitulo){
     let actionSheet = this.actionSheetCtrl.create({
       title: 'O que você quer fazer?',
       buttons: [
-        {
-          text: 'Deletar Música',
-          role: 'destructive',
-          handler: () => {
-            this.removerMusica(musicaId);
-          }
-        },{
-          text: 'Atualizar Música',
-          handler: () => {
-            this.atualizarMusica(musicaId, musicaTitulo);
-          }
-        },{
-          text: 'Cancelar',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancelar clicado');
-          }
+      {
+        text: 'Deletar Música',
+        role: 'destructive',
+        handler: () => {
+          this.removerMusica(musicaId);
         }
+      },{
+        text: 'Atualizar Música',
+        handler: () => {
+          this.atualizarMusica(musicaId, musicaTitulo);
+        }
+      },{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar clicado');
+        }
+      }
       ]
     });
     actionSheet.present();
@@ -56,16 +95,16 @@ export class HomePage {
 
   atualizarMusica(musicaId: string, musicaTitulo: string){
     let prompt = this.alertCtrl.create({
-    title: 'Noma da Música',
-    message: "Atualizar Música",
-    inputs: [
+      title: 'Noma da Música',
+      message: "Atualizar Música",
+      inputs: [
       {
         name: 'tituloVar',
         placeholder: 'Título',
         value: musicaTitulo
       },
-    ],
-    buttons: [
+      ],
+      buttons: [
       {
         text: 'Cancelar',
         handler: data => {
@@ -80,38 +119,38 @@ export class HomePage {
           });
         }
       }
-    ]
-  });
-  prompt.present();
+      ]
+    });
+    prompt.present();
   }
 
   adicionarMusicas(){
-        let prompt = this.alertCtrl.create({
-        title: 'Nome da Música',
-        message: "Entre com um nome para esta nova música ser adicionada",
-        inputs: [
-          {
-            name: 'titulo',
-            placeholder: 'Título'
-          },
-        ],
-        buttons: [
-          {
-            text: 'Cancelar',
-            handler: data => {
-              console.log('Cancelar Clicado');
-            }
-          },
-          {
-            text: 'Salvar',
-            handler: data => {
-              this.musicas.push({
-                titulo: data.titulo
-              });
-            }
-          }
-        ]
-      });
-      prompt.present();
+    let prompt = this.alertCtrl.create({
+      title: 'Nome da Música',
+      message: "Entre com um nome para esta nova música ser adicionada",
+      inputs: [
+      {
+        name: 'titulo',
+        placeholder: 'Título'
+      },
+      ],
+      buttons: [
+      {
+        text: 'Cancelar',
+        handler: data => {
+          console.log('Cancelar Clicado');
+        }
+      },
+      {
+        text: 'Salvar',
+        handler: data => {
+          this.musicas.push({
+            titulo: data.titulo
+          });
+        }
+      }
+      ]
+    });
+    prompt.present();
   }
 }
